49f4ed46715b32aafc26875f5e50e806da8e304b
Aliya Gaeva <a.gaeva@Gaeva-SD-01845.local>
2022-07-12 11:02:00 +0500

    refactor_logo

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="page">
       <main className="page__main">
         <section>
           <h1>404. Page not found</h1>
-          <a href="/">Вернуться на главную</a>
+          <Link to="/">Вернуться на главную</Link>
         </section>
       </main>
     </div>
