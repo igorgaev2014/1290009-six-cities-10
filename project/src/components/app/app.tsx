ed671320544ec5742d3bdaa4f5e61d2c96c4237d
Aliya Gaeva <a.gaeva@Gaeva-SD-01845.local>
2022-07-12 10:48:28 +0500

    components_routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import RoomScreen from '../../pages/room-screen/room-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   placesCount: number;
@@ -6,7 +13,15 @@ type AppScreenProps = {
 
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen placesCount={placesCount}/>}></Route>
+        <Route path={AppRoute.Login} element={<LoginScreen/>}></Route>
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen/>}></Route>
+        <Route path={AppRoute.Room} element={<RoomScreen/>}></Route>
+        <Route path="*" element={<NotFoundScreen/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
