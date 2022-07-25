2201c1ef882069deb5e8252ce3d22f7e68e22e39
Игорь Гаев <igorgaev2014@gmail.com>
2022-07-25 15:19:15 +0300

    leaflet usemap check fix

--- project/src/hooks/useMap.ts
+++ project/src/hooks/useMap.ts
@@ -10,7 +10,7 @@ function useMap(
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
-    if (mapRef.current !== null && isRenderedRef.current) {
+    if (mapRef.current !== null && !isRenderedRef.current) {
       const instance = new Map(mapRef.current, {
         center: {
           lat: city.location.latitude,
