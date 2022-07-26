c924f0cec6bf3dfd5a57825794259dbc6aeffbfb
Игорь Гаев <igorgaev2014@gmail.com>
2022-07-26 14:28:37 +0300

    Added stars rating to form

--- project/src/components/reviews-form/reviews-form.tsx
+++ project/src/components/reviews-form/reviews-form.tsx
@@ -1,7 +1,14 @@
-import { ChangeEvent, useState, FormEvent } from 'react';
+import { ChangeEvent, useState, FormEvent, useCallback } from 'react';
+import { STAR_RATING } from '../../const';
+import Stars from '../stars/stars';
 
 function ReviewsForm(): JSX.Element {
-  const [formData, setFormData] = useState({review: ''});
+  const [formData, setFormData] = useState({rating: '', review: ''});
+
+  const memoChangeHandler = useCallback((evt: ChangeEvent<HTMLInputElement>) => {
+    const {value} = evt.target;
+    setFormData({...formData, rating: value});
+  }, [formData]);
 
   function onTextAreaChange(evt: ChangeEvent<HTMLTextAreaElement>) {
     const {value} = evt.target;
@@ -16,40 +23,9 @@ function ReviewsForm(): JSX.Element {
     <form className="reviews__form form" action="#" method="post" onSubmit={onSubmitHandler}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {STAR_RATING.map((star, i) => (
+          <Stars id={i} key={star} title={star} onChangeStar={memoChangeHandler}/>
+        )).reverse()}
       </div>
       <textarea
         className="reviews__textarea form__textarea"
@@ -69,3 +45,4 @@ function ReviewsForm(): JSX.Element {
 }
 
 export default ReviewsForm;
+
