0bcb75f88eafda75444f5e4ef9da98e3c15f5264
Игорь Гаев <igorgaev2014@gmail.com>
2022-07-26 13:30:27 +0300

    Added Reviews amount

--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -130,7 +130,7 @@ function RoomScreen({offers, reviews}: RoomScreenProps): JSX.Element {
                 </div>
               </div>
               <section className="property__reviews reviews">
-                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+                <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
                 <ReviewsList reviews={reviews} />
                 <ReviewsForm />
               </section>
